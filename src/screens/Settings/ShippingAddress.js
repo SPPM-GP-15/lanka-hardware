d7e7825d131e6a0128f1bb5e01e13dab328f70bb
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-07-11 21:48:12 +0530

    Small changes on Settings UI

--- src/screens/Settings/ShippingAddress.js
+++ src/screens/Settings/ShippingAddress.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { CheckBox } from "react-native-elements";
@@ -40,30 +46,33 @@ export default function ShippingAddress() {
 
   return (
     <View style={styles.container}>
-      {addresses.map((address, index) => (
-        <View key={index} style={styles.addressCard}>
-          <View style={styles.addressInfo}>
-            <Text style={styles.name}>{address.name}</Text>
-            <Text style={styles.address}>{address.address}</Text>
+      <Text style={styles.header}>Address</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {addresses.map((address, index) => (
+          <View key={index} style={styles.addressCard}>
+            <View style={styles.addressInfo}>
+              <Text style={styles.name}>{address.name}</Text>
+              <Text style={styles.address}>{address.address}</Text>
+            </View>
+            <View style={styles.actions}>
+              <TouchableOpacity
+                onPress={() => handleCheck(index)}
+                style={styles.checkButton}
+              ></TouchableOpacity>
+              <TouchableOpacity style={styles.editButton}>
+                <Text style={styles.buttonText}>Edit</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <View style={styles.actions}>
-            <TouchableOpacity
-              onPress={() => handleCheck(index)}
-              style={styles.checkButton}
-            ></TouchableOpacity>
-            <TouchableOpacity style={styles.editButton}>
-              <Text style={styles.buttonText}>Edit</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      ))}
+        ))}
+      </ScrollView>
       <TouchableOpacity
         style={styles.addButton}
         onPress={() => {
           navigation.navigate("AddAddress");
         }}
       >
-        <Text style={[styles.buttonText, { fontSize: 30 }]}>+</Text>
+        <Text style={{ fontSize: 30, color: "white" }}>+</Text>
       </TouchableOpacity>
     </View>
   );
@@ -74,13 +83,18 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
   addressCard: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginBottom: 15,
-    padding: 10,
-    backgroundColor: "#f5f5f5",
+    marginVertical: 10,
+    padding: 20,
+    backgroundColor: "#dddd",
     borderRadius: 8,
   },
   addressInfo: {
@@ -91,7 +105,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   address: {
-    fontSize: 14,
+    fontSize: 13,
+    color: "#333",
+    marginTop: 5,
+    width: "80%",
   },
   actions: {
     flexDirection: "row",
@@ -102,18 +119,16 @@ const styles = StyleSheet.create({
   },
   editButton: {
     padding: 5,
-    backgroundColor: "#007bff",
     borderRadius: 5,
   },
   buttonText: {
-    color: "#fff",
+    color: "#071952",
   },
   addButton: {
-    backgroundColor: "#007bff",
-    borderRadius: 5,
+    backgroundColor: "#405D72",
+    borderRadius: 10,
     alignItems: "center",
-    marginLeft: 50,
-    marginRight: 50,
     justifyContent: "center",
+    marginBottom: 40,
   },
 });
