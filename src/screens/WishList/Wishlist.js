3999ee4adc3b3f0792266995976128d250bdfbca
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-07-15 05:38:44 +0530

    Merge from Wishlisht and Detail product

--- src/screens/WishList/Wishlist.js
+++ src/screens/WishList/Wishlist.js
@@ -1,16 +1,15 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { ScrollView } from "react-native";
 import ProductCard from "../../components/wishlist-product/ProductCard";
 
 export default function Wishlist() {
   return (
-    <ScrollView
-      vertical
-      showsVerticalScrollIndicator={true}
-      style={styles.container}
-    >
-      <Text style={styles.txt}>Products</Text>
+    <ScrollView showsVerticalScrollIndicator={true} style={styles.container}>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
       <ProductCard />
       <ProductCard />
       <ProductCard />
@@ -19,38 +18,9 @@ export default function Wishlist() {
 }
 
 const styles = StyleSheet.create({
+  screen: {},
   container: {
-    flex: 1,
-  },
-  txt: {
-    marginVertical: 20,
-    fontSize: 20,
-    marginLeft: 20,
-    fontWeight: "600",
-  },
-  productCard: {
-    backgroundColor: "#fff",
-    borderRadius: 8,
-    padding: 16,
-    marginBottom: 16,
-  },
-  productImage: {
-    width: "100%",
-    height: 150,
-    borderRadius: 8,
-    marginBottom: 8,
-  },
-  productName: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 4,
-  },
-  productDescription: {
-    fontSize: 14,
-    marginBottom: 8,
-  },
-  productPrice: {
-    fontSize: 16,
-    fontWeight: "bold",
+    marginBottom: 20,
+    marginTop: 30,
   },
 });
